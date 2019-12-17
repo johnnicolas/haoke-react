effb9c43a00d93eda7b43f745a7b35d4c7a15150
johnnicolas <453226483@qq.com>
2019-12-17 12:35:41 +0800

    完成二级渲染覆盖物

--- renting-house-42/src/views/Map/index.jsx
+++ renting-house-42/src/views/Map/index.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import styles from "./index.module.scss";
 import NavHeader from "../../components/NavHeader";
 import { getlocationCity } from "../../untils/city";
+//引入loading
+import {Toast} from 'antd-mobile'
 const BMap = window.BMap;
 //圆形覆盖物的样式
 const labelStyle = {
@@ -16,6 +18,30 @@ const labelStyle = {
 export default class Map extends Component {
   componentDidMount() {
     this.getmap();
+  }
+  //这个是判断是1级还是2级的方法
+  getTypeAndZoom= ()=>{
+    let type = 'circle' //圆形覆盖物适用 一二级
+    let nextZoom = 11   //缩放等级
+    //获取当前的缩放级别，参考类型里面的zoom
+    const currenZoom = this.map.getZoom()
+    //然后进行判断
+    if (currenZoom > 10 && currenZoom<12) {
+      type = 'circle'
+      nextZoom = 13
+    } else if (currenZoom > 12 && currenZoom<14){
+      type = 'circle'
+      nextZoom = 15
+    }else if(currenZoom > 15){
+     type = 'rect'
+    }
+    //将这两个参数返回
+  return {
+    type,
+    nextZoom
+  }
+    
+    
   }
   //创建一个方法获取map
   getmap = async () => {
@@ -44,10 +70,23 @@ export default class Map extends Component {
   };
   //渲染覆盖物 然后将这个覆盖物渲染到上面
   renderOverlays = async (id) => {
+    Toast.loading('正在拼命加载中',1)
     const res = await this.$axios.get(`/area/map?id=${id}`);
     console.log("这是获取城市的id", res);
+    //等到渲染完毕之后移除toast
+    Toast.hide()
+    //申明一个常量利用解构的语法取出里面的上面获取缩放级别的方法
+    const {type,nextZoom} = this.getTypeAndZoom()
     res.data.body.forEach(item => {
-      //根据item生成一个一个的覆盖物，并且创建好之后，添加到地图上,利用解构语法
+      if (type === 'circle') {
+        //将二级覆盖物的方法传入item和nextzoom
+        this.renderCircleOverlays(item,nextZoom)
+      }
+    });
+  };
+  //添加第一级和第二级覆盖物
+  renderCircleOverlays=(item,nextZoom)=>{
+ //根据item生成一个一个的覆盖物，并且创建好之后，添加到地图上,利用解构语法
       //前面一个一定要放经度
       const {
         coord: { longitude, latitude },
@@ -67,8 +106,25 @@ export default class Map extends Component {
       <p class=${styles.name}>${count}</p>
       </div>`);
       this.map.addOverlay(label);
-    });
-  };
+      //给每个覆盖物设置点击事件
+      label.addEventListener('click',()=>{
+        //清除之前的覆盖物，但是这里要使用的是异步方法，因为等同步都请求完之后，在执行异步的代码
+        setTimeout(() => {
+          //清除覆盖物的方法
+          this.map.clearOverlays()
+        }, 0);
+        //在渲染新的视图,并且将缩放级别传入就是nextzoom
+        this.map.centerAndZoom(point, nextZoom)
+        //渲染地图
+       //调用获取小区地址的方法将小区的value传
+       this.renderOverlays(value)
+      })
+      this.map.addOverlay(label);
+  }
+  //添加第三级覆盖物
+  renderRectOverlays=()=>{
+
+  }
   render() {
     return (
       <div className={styles.map}>
